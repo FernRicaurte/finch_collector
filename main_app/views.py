c0c09e77ef4352840c1ecc70a9a72bb0ec3ce8e1
FernRicaurte <FernRicaurte2@gmail.com>
2023-06-06 10:45:13 -0400

    pt.5 complete

--- main_app/views.py
+++ main_app/views.py
@@ -1,47 +1,67 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from .models import Finch
+from django.views.generic import ListView, DetailView
+from .models import Finch, Toy
 from .forms import FeedingForm
 
-# Define the home view
 def home(request):
-  # Include an .html file extension 
-  return render(request, 'home.html')
+    return render(request, 'home.html')
 
-# Define the about view
 def about(request):
-  # Include an .html file extension 
-  return render(request, 'about.html')
+    return render(request, 'about.html')
 
-# Define the finches_index view
 def finches_index(request):
     finches = Finch.objects.all()
     return render(request, 'finches/index.html', {'finches': finches})
 
 def finches_detail(request, finch_id):
-  finch = Finch.objects.get(id=finch_id)
-  feeding_form = FeedingForm()
-  return render(request, 'finches/detail.html', { 'finch': finch, 'feeding_form': feeding_form})
-  
+    finch = Finch.objects.get(id=finch_id)
+    id_list = finch.toys.all().values_list('id')
+    toy_finch_doesnt_have = Toy.objects.exclude(id__in=id_list)
+    feeding_form = FeedingForm()
+    return render(request, 'finches/detail.html', { 'finch': finch, 'feeding_form': feeding_form, 'toys': toy_finch_doesnt_have })
+
 def add_feeding(request, finch_id):
     form = FeedingForm(request.POST)
     if form.is_valid():
-      new_feeding = form.save(commit=False)
-    new_feeding.finch_id = finch_id
-    new_feeding.save()
+        new_feeding = form.save(commit=False)
+        new_feeding.finch_id = finch_id
+        new_feeding.save()
+    return redirect('detail', finch_id=finch_id)
+
+def assoc_toy(request, finch_id, toy_id):
+    Finch.objects.get(id=finch_id).toys.add(toy_id)
     return redirect('detail', finch_id=finch_id)
 
 class FinchCreate(CreateView):
-  model = Finch
-  fields = '__all__'
-  
+    model = Finch
+    fields = ['name', 'breed', 'description', 'age']
+
 class FinchUpdate(UpdateView):
-  model = Finch
-  fields = ['description', 'age']
+    model = Finch
+    fields = ['description', 'age']
 
 class FinchDelete(DeleteView):
-  model = Finch
-  success_url = '/finches/index'
+    model = Finch
+    success_url = '/finches/'
+
+class ToyCreate(CreateView):
+    model = Toy
+    fields = ['name', 'color']
+
+class ToyList(ListView):
+    model = Toy
+
+class ToyDetail(DetailView):
+    model = Toy
+
+class ToyUpdate(UpdateView):
+    model = Toy
+    fields = ['name', 'color']
+
+class ToyDelete(DeleteView):
+    model = Toy
+    success_url = '/toys/'
 
 
 
