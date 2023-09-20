0c2f10ae35100f1a6b24ce72dfbb23d5d10be92d
FernRicaurte <FernRicaurte2@gmail.com>
2023-09-20 11:24:36 -0400

    new superuser credentials

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.urls import  reverse
+from django.urls import reverse
 
 class Toy(models.Model):
     name = models.CharField(max_length=50)
@@ -23,7 +23,7 @@ class Finch(models.Model):
         return self.name
     
     def get_absolute_url(self):
-        return  reverse('detail', kwargs={'finch_id': self.id})
+        return reverse('detail', kwargs={'finch_id': self.id})
     
 
 class Feeding(models.Model):
